8563761321fe95e0ad7c187880e0aa5802fbaada
OGENDI <demokonge17@gmail.com>
2022-01-19 11:04:31 +0300

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisplaypgComponent } from './displaypg/displaypg.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'index', pathMatch:'full'},
+  {path:'index', component:HomepageComponent},
+  {path: 'display/:query', component:DisplaypgComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
